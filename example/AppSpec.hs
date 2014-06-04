54cac52a010c5938e2b98b666ebbb9e5dacb3648
Fujimura Daisuke <me@fujimuradaisuke.com>
2014-06-04 18:55:14 +0900

    Add example

---
+++ example/AppSpec.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module ExampleSpec (spec) where
+
+import           Test.Hspec
+import           Test.Hspec.Wai
+
+import           Network.HTTP.Types (status200)
+import           Network.Wai        (Application, responseLBS)
+
+app :: Application
+app _ = return $ responseLBS status200 [("Content-Type", "text/plain")] "hello"
+
+run :: Application -> IO Application
+run = return
+
+spec :: Spec
+spec = before (run app) $ do
+  describe "GET /foo" $ do
+    it "reponds with 200" $ do
+      get "/foo" `shouldRespondWith` 200
+
+    it "reponds with 'hello'" $ do
+      get "/foo" `shouldRespondWith` "hello"
+
+    it "reponds with 200 / 'bar'" $ do
+      get "/foo" `shouldRespondWith` "hello" {matchStatus = 200}
