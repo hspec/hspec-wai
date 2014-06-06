2e4a6c709b2248d75c13c37726554f2e85da1554
Fujimura Daisuke <me@fujimuradaisuke.com>
2014-06-06 22:40:43 +0900

    Documentation

--- src/Test/Hspec/Wai.hs
+++ src/Test/Hspec/Wai.hs
@@ -32,25 +32,48 @@ import           Network.Wai (Request(..))
 import           Test.Hspec.Wai.Internal
 import           Test.Hspec.Wai.Matcher
 
+-- |
+-- Passes if the given `Header` exists in the response.
 shouldHaveHeader :: WaiSession SResponse -> Header -> WaiExpectation
 shouldHaveHeader action header = do
   r <- action
   forM_ (haveHeader r header) (liftIO . expectationFailure)
 
+-- |
+-- Passs if
+--
+--   * the given number matches with the HTTP Status code of the response.
+--   * the given string matches with the body of the response.
+--   * the given `ResponseMatcher` matches with the response.
+--
+-- Example:
+--
+-- > get "/foo" `shouldRespondWith` 200                         -- Pass
+-- > get "/foo" `shouldRespondWith` "bar"                       -- Pass if the body is "bar"
+-- > get "/foo" `shouldRespondWith` "bar" { matchStatus = 200 } -- Pass if the body is "bar" and status is 200
+--
 shouldRespondWith :: WaiSession SResponse -> ResponseMatcher -> WaiExpectation
 shouldRespondWith action matcher = do
   r <- action
   forM_ (match r matcher) (liftIO . expectationFailure)
 
+-- |
+-- | Performs `GET` request to running app.
 get :: ByteString -> WaiSession SResponse
 get p = request methodGet p ""
 
+-- |
+-- | Performs `POST` request to running app.
 post :: ByteString -> LB.ByteString -> WaiSession SResponse
 post = request methodPost
 
+-- |
+-- | Performs `PUT` request to running app.
 put :: ByteString -> LB.ByteString -> WaiSession SResponse
 put = request methodPut
 
+-- |
+-- | Performs request to running app, with HTTP Method, path and body.
 request :: Method -> ByteString -> LB.ByteString -> WaiSession SResponse
 request m p b = getApp >>= liftIO . runSession (Wai.srequest $ SRequest req b)
   where
