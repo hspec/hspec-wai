dcc5fc875012467ca55d179462f840993f79dd4c
Simon Hengel <sol@typeful.net>
2014-06-13 13:23:39 +0800

    More documentation

--- src/Test/Hspec/Wai.hs
+++ src/Test/Hspec/Wai.hs
@@ -1,12 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
+-- | Have a look at the <https://github.com/hspec/hspec-wai#readme README> for
+-- an example of how to use this library.
 module Test.Hspec.Wai (
-  with
+-- * Types
+  WaiSession
+, WaiExpectation
+
+-- * Performing requests
 , get
 , post
 , put
 , request
+
+-- * Matching on the response
 , shouldRespondWith
 , ResponseMatcher(..)
+
+-- * Helpers
+, with
 ) where
 
 import           Data.Foldable
@@ -22,41 +33,63 @@ import           Test.Hspec
 import           Test.Hspec.Wai.Internal
 import           Test.Hspec.Wai.Matcher
 
+-- | An alias for `before`.
 with :: IO a -> SpecWith a -> Spec
 with = before
 
--- |
--- Passs if
+-- | Set the expectation that a response matches a specified `ResponseMatcher`.
 --
---   * the given number matches with the HTTP Status code of the response.
---   * the given string matches with the body of the response.
---   * the given `ResponseMatcher` matches with the response.
+-- A @ResponseMatcher@ matches a response if:
 --
--- Example:
+-- * the specified status matches the HTTP response status code
 --
--- > get "/foo" `shouldRespondWith` 200                         -- Pass
--- > get "/foo" `shouldRespondWith` "bar"                       -- Pass if the body is "bar"
--- > get "/foo" `shouldRespondWith` "bar" { matchStatus = 200 } -- Pass if the body is "bar" and status is 200
+-- * the specified body (if any) matches the response body
 --
-
+-- * the response has all of the specified `Header` fields
+--   (the response may have arbitrary additional `Header` fields)
+--
+-- You can use @ResponseMatcher@'s (broken) `Num` instance to match for a HTTP
+-- status code:
+--
+-- > get "/" `shouldRespondWith` 200
+-- > -- matches if status is 200
+--
+-- You can use @ResponseMatcher@'s `IsString` instance to match for a HTTP
+-- status @200@ and a body:
+--
+-- > get "/" `shouldRespondWith` "foo"
+-- > -- matches if body is "foo" and status is 200
+--
+-- If you want to match for a different HTTP status, you can use record update
+-- notation to specify `matchStatus` explicitly:
+--
+-- > get "/" `shouldRespondWith` "foo" {matchStatus = 404}
+-- > -- matches if body is "foo" and status is 404
+--
+-- If you want to require a specific header field you can specify
+-- `matchHeaders`:
+--
+-- > get "/" `shouldRespondWith` "foo" {matchHeaders = "Content-Type: text/plain"}
+-- > -- matches if body is "foo", status is 200 and ther is a header field "Content-Type: text/plain"
 shouldRespondWith :: WaiSession SResponse -> ResponseMatcher -> WaiExpectation
 shouldRespondWith action matcher = do
   r <- action
   forM_ (match r matcher) (liftIO . expectationFailure)
 
--- | Performs `GET` request to running app.
+-- | Perform a @GET@ request to the application under test.
 get :: ByteString -> WaiSession SResponse
 get path = request methodGet path ""
 
--- | Performs `POST` request to running app.
+-- | Perform a @POST@ request to the application under test.
 post :: ByteString -> LB.ByteString -> WaiSession SResponse
 post = request methodPost
 
--- | Performs `PUT` request to running app.
+-- | Perform a @PUT@ request to the application under test.
 put :: ByteString -> LB.ByteString -> WaiSession SResponse
 put = request methodPut
 
--- | Performs request to running app, with HTTP Method, path and body.
+-- | Perform a request to the application under test, with specified HTTP
+-- method, request path and body.
 request :: Method -> ByteString -> LB.ByteString -> WaiSession SResponse
 request method path body = getApp >>= liftIO . runSession (Wai.srequest $ SRequest req body)
   where
