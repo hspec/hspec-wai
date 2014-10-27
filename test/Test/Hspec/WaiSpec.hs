7f02cf16933ecb5479d37f893a0442701eca0110
Jon Childress <jon@childr.es>
2014-10-27 11:41:28 -0500

    qualify all Test.Hspec imports

--- test/Test/Hspec/WaiSpec.hs
+++ test/Test/Hspec/WaiSpec.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Test.Hspec.WaiSpec (main, spec) where
 
-import Test.Hspec
+import qualified Test.Hspec as Hspec
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BL
 import Network.HTTP.Types
@@ -10,20 +10,20 @@ import Network.Wai
 import Test.Hspec.Wai
 
 main :: IO ()
-main = hspec spec
+main = Hspec.hspec spec
 
 expectMethod :: Method -> Application
 expectMethod method req respond = do
-  requestMethod req `shouldBe` method
+  requestMethod req `Hspec.shouldBe` method
   respond $ responseLBS status200 [] ""
 
 expectRequest :: Method -> ByteString -> ByteString -> [Header] -> Application
 expectRequest method path body headers req respond = do
-  requestMethod req `shouldBe` method
-  rawPathInfo req `shouldBe` path
-  requestHeaders req `shouldBe` headers
+  requestMethod req `Hspec.shouldBe` method
+  rawPathInfo req `Hspec.shouldBe` path
+  requestHeaders req `Hspec.shouldBe` headers
   rawBody <- requestBody req
-  rawBody `shouldBe` body
+  rawBody `Hspec.shouldBe` body
   respond $ responseLBS status200 [] ""
 
 spec :: Spec
