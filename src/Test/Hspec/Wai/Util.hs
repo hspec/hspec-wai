2475e567a2bf42b14a7f53beb138b2b7de4c1bd2
Simon Hengel <sol@typeful.net>
2014-11-16 15:26:27 +0800

    Add formUrlEncodeQuery

--- src/Test/Hspec/Wai/Util.hs
+++ src/Test/Hspec/Wai/Util.hs
@@ -4,17 +4,23 @@ module Test.Hspec.Wai.Util where
 import           Control.Monad
 import           Data.Monoid
 import           Data.Maybe
-import           Data.Char
+import           Data.List
+import           Data.Word
+import           Data.Char hiding (ord)
+import qualified Data.Char as Char
 import           Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy as LB
+import           Data.ByteString.Lazy.Builder (Builder)
+import qualified Data.ByteString.Lazy.Builder as Builder
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.CaseInsensitive as CI
 import           Network.HTTP.Types
 
 formatHeader :: Header -> String
-formatHeader header@(name, value) = "  " ++ fromMaybe (show header) (safeToString $ B.concat [CI.original name, ": ", value])
+formatHeader header@(name, value) = "  " ++ fromMaybe (show header) (safeToString $ B8.concat [CI.original name, ": ", value])
 
 safeToString :: ByteString -> Maybe String
 safeToString bs = do
@@ -27,3 +33,57 @@ safeToString bs = do
 -- for compatibility with older versions of `bytestring`
 toStrict :: LB.ByteString -> ByteString
 toStrict = mconcat . LB.toChunks
+
+formUrlEncodeQuery :: [(String, String)] -> LB.ByteString
+formUrlEncodeQuery = Builder.toLazyByteString . mconcat . intersperse amp . map encodePair
+  where
+    equals = Builder.word8 (ord '=')
+    amp = Builder.word8 (ord '&')
+    percent = Builder.word8 (ord '%')
+    plus = Builder.word8 (ord '+')
+
+    encodePair :: (String, String) -> Builder
+    encodePair (key, value) = encode key <> equals <> encode value
+
+    encode :: String -> Builder
+    encode = escape . T.encodeUtf8 . T.pack . newlineNormalize
+
+    newlineNormalize :: String -> String
+    newlineNormalize input = case input of
+      [] -> []
+      '\n' : xs -> '\r' : '\n': newlineNormalize xs
+      x : xs -> x : newlineNormalize xs
+
+    escape :: ByteString -> Builder
+    escape = mconcat . map f . B.unpack
+      where
+        f :: Word8 -> Builder
+        f c
+          | p c = Builder.word8 c
+          | c == ord ' ' = plus
+          | otherwise = percentEncode c
+
+        p :: Word8 -> Bool
+        p c =
+             ord 'a' <= c && c <= ord 'z'
+          || c == ord '_'
+          || c == ord '*'
+          || c == ord '-'
+          || c == ord '.'
+          || ord '0' <= c && c <= ord '9'
+          || ord 'A' <= c && c <= ord 'Z'
+
+    ord :: Char -> Word8
+    ord = fromIntegral . Char.ord
+
+    percentEncode :: Word8 -> Builder
+    percentEncode n = percent <> hex hi <> hex lo
+      where
+        (hi, lo) = n `divMod` 16
+
+    hex :: Word8 -> Builder
+    hex n = Builder.word8 (offset + n)
+      where
+        offset
+          | n < 10    = 48
+          | otherwise = 55
--- test/Test/Hspec/Wai/UtilSpec.hs
+++ test/Test/Hspec/Wai/UtilSpec.hs
@@ -2,13 +2,58 @@
 module Test.Hspec.Wai.UtilSpec (main, spec) where
 
 import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
 import           Test.Hspec.Wai.Util
+import           Data.Monoid
+import           Network.HTTP.Types (parseSimpleQuery)
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
 main :: IO ()
 main = hspec spec
 
+decodePair :: (ByteString, ByteString) -> (String, String)
+decodePair (a, b) = (decode a, decode b)
+  where
+    decode :: ByteString -> String
+    decode = newlineNormalize . T.unpack . T.decodeUtf8
+
+    newlineNormalize :: String -> String
+    newlineNormalize input = case input of
+      [] -> []
+      '\r' : '\n' : xs -> '\n': newlineNormalize xs
+      x : xs -> x : newlineNormalize xs
+
 spec :: Spec
 spec = do
+  describe "formUrlEncodeQuery" $ do
+    it "separates keys from values by =" $ do
+      formUrlEncodeQuery [("foo", "bar")] `shouldBe` "foo=bar"
+
+    it "separates pairs by &" $ do
+      formUrlEncodeQuery [("foo", "bar"), ("foo", "baz")] `shouldBe` "foo=bar&foo=baz"
+
+    it "applies newline normalization" $ do
+      formUrlEncodeQuery [("text", "foo\nbar\nbaz\n")] `shouldBe` "text=foo%0D%0Abar%0D%0Abaz%0D%0A"
+
+    it "handles Unicode characters" $ do
+      formUrlEncodeQuery [("foo","bar-\955-baz")] `shouldBe` "foo=bar-%CE%BB-baz"
+
+    context "when encoding characters in the printable ASCII range" $ do
+      let input = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+          reference = "+%21%22%23%24%25%26%27%28%29*%2B%2C-.%2F0123456789%3A%3B%3C%3D%3E%3F%40ABCDEFGHIJKLMNOPQRSTUVWXYZ%5B%5C%5D%5E_%60abcdefghijklmnopqrstuvwxyz%7B%7C%7D%7E"
+
+      it "behaves like Firefox" $ do
+        formUrlEncodeQuery [("foo", input)] `shouldBe` "foo=" <> reference
+
+    modifyMaxSize (`div` 5) $ do
+      it "can be reverted by Network.HTTP.Types.parseSimpleQuery" $ do
+        property $ \xs -> do
+          (map decodePair . parseSimpleQuery . LB.toStrict . formUrlEncodeQuery) xs `shouldBe` xs
+
   describe "safeToString" $ do
     context "when used on an empty string" $ do
       it "returns Nothing" $ do
